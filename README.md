de98c66dfb10d0fb7d2b55dae39bfb4babe45a54
Ahmed Kandeel <Ahmed.Kandeel@itworx.com>
2018-11-22 11:31:12 +0200

    files

---
+++ README.md
@@ -0,0 +1,20 @@
+# MyReads Project
+## Introduction about the App:
+- Through this project, a user can create their own personal library.
+
+- The user can further categorize the books as "Currently Reading", or "Read" or as "Want to Read".
+
+- These books can be organized in different Shelves.
+
+## Functionality of the App:
+- The functionality is implemented using "Book" and "Shelf" components in this project.
+![Screenshot](https://d17h27t6h515a5.cloudfront.net/topher/2017/May/590c0f12_react-project1-a/react-project1-a.png)
+
+- The API server and client library provided by Udacity have been used to persist information as the user interacts with the application.
+
+
+
+## Installation and Launching the App:
+- Use the command "npm install" to install all the node packages that are required for the application.
+- Once all the packages are downloaded, use "npm start" to launch the application.
+- Once the application is launched, we can see that the application is up and running on localhost, port 3000 and can be accessed using the URL - http://localhost:3000/ with the title as "My Reads".
---
+++ src/Book.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { PropTypes } from 'prop-types'
+class Book extends Component {
+
+    static propTypes = {
+        book: PropTypes.object.isRequired,
+        onBookShelfChange: PropTypes.func.isRequired
+    };
+    changebookShelf = e =>
+        this.props.onBookShelfChange(this.props.book, e.target.value);
+    render() {
+        //Error handling if author or thumbnail returned is empty or null
+        const { book} = this.props;
+        // add fallbacks for missing cover images and title
+        const coverImg = book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : '';
+        const title = book.title ? book.title : 'No title available';
+        return (
+                <div className="book">
+                    <div className="book-top">
+                        <div className="book-cover"
+                            style={{ width: 128, height: 193, backgroundImage: `url(${coverImg})`  }}></div>
+                        <div className="book-shelf-changer">
+                            <select onChange={this.changebookShelf} >
+                                <option value="none" disabled>Move to...</option>
+                                 <option value="currentlyReading">Currently Reading</option>
+                                 <option value="wantToRead">need to Read</option>
+                                 <option value="read">Readed</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="book-title">{title}</div>
+                    <div className="book-authors">
+                        {book.authors}
+                    </div>
+                </div>
+        );
+    }
+}
+
+export default Book;
