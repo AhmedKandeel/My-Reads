b151284c54aedcd5829588b5d68fd50dde95a345
Ahmed Kandeel <ahmed-kandeel@outlook.com>
2018-11-23 19:59:04 +0200

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -1,41 +1,32 @@
-import React, { Component } from 'react'
-import { PropTypes } from 'prop-types'
-class Book extends Component {
+import React from 'react'
 
-    static propTypes = {
-        book: PropTypes.object.isRequired,
-        onBookShelfChange: PropTypes.func.isRequired
-    };
-    changebookShelf = e =>
-        this.props.onBookShelfChange(this.props.book, e.target.value);
-    render() {
-        //Error handling if author or thumbnail returned is empty or null
-        const { book} = this.props;
-        // add fallbacks for missing cover images and title
-        const coverImg = book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : '';
-        const title = book.title ? book.title : 'No title available';
-        return (
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover"
-                            style={{ width: 128, height: 193, backgroundImage: `url(${coverImg})`  }}></div>
-                        <div className="book-shelf-changer">
-                            <select onChange={this.changebookShelf} >
-                                <option value="none" disabled>Move to...</option>
-                                 <option value="currentlyReading">Currently Reading</option>
-                                 <option value="wantToRead">need to Read</option>
-                                 <option value="read">Readed</option>
-                                 <option value="none" disabled>None</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="book-title">{title}</div>
-                    <div className="book-authors">
-                        {book.authors}
-                    </div>
+const Book = (props) => {
+    //Error handling if author or thumbnail returned is empty or null
+    let url = (props.book.imageLinks) ? 'url(' + props.book.imageLinks.thumbnail + ')' : '';
+    let auth = (props.book.authors && props.book.authors.length > 0) ? props.book.authors : [];
+
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover"
+                     style={{width: 128, height: 193, backgroundImage: url.toString()}}></div>
+                <div className="book-shelf-changer">
+                    <select onChange={(e) => props.onBookShelfChange(props.book, e.target.value)}
+                            value={props.book.shelf}>
+                        <option value="" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
                 </div>
-        );
-    }
+            </div>
+            <div className="book-title">{props.book.title}</div>
+            <div className="book-authors">
+                {Array.isArray(auth)?auth.join(', '):''}
+            </div>
+        </div>
+    );
 }
 
 export default Book;
